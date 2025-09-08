423b01fa1f441f154d084a34e5d6de54c5368554
ronit <ronitkhorjuvekaronlyeducation@gmail.com>
2025-09-08 14:51:49 +0530

    Google Tag Manager in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,8 +48,27 @@ export default function RootLayout({
             gtag('config', 'G-QVHJMEGY1T');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KWQ2B6FX');
+          `}
+        </Script>
       </head>
       <body style={{ fontFamily: 'var(--font-clash-display), sans-serif' }}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KWQ2B6FX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="google manager"
+          />
+        </noscript>
         <AppRouterCacheProvider options={{ key: 'css', enableCssLayer: true }}>
           <MuiThemeProvider>
             <SnackbarProvider>
