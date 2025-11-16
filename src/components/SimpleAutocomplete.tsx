ed3287835cb9c4c13a7f4a73620c1b5efce8a94e
Ali Shaikh <qasimshaikh730@gmail.com>
2025-11-16 18:19:43 +0530

    unused code removed

--- src/components/SimpleAutocomplete.tsx
+++ src/components/SimpleAutocomplete.tsx
@@ -1,27 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-
-import React, { useState } from 'react';
+import React from 'react';
 import { UseFormSetValue, UseFormWatch } from 'react-hook-form';
 import { ContactFormScehmaType } from '@/types/ContactForm';
-
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-];
 
 export default function SimpleAutocomplete({
   setValue,
